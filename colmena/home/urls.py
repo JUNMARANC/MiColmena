c54c0d5662db6f46196dba9ebbf29a237817b7bb
JUNMARANC <juanku690@gmail.com>
2026-06-19 20:32:42 -0500

    cambios en el Home "Contactanos"

--- colmena/home/urls.py
+++ colmena/home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from home.views import inicio,quienes,servicios,contactanos
+from home.views import inicio,quienes,servicios,contactanos,correo
 
 urlpatterns = [
-    path('inicio',inicio,name="Inicio"),
+    path('',inicio,name="Inicio"),
     path('quienes',quienes,name="Quienes"),
     path('servicios',servicios,name="Servicios"),
     path('contactanos', contactanos,name="Contactanos"),
+    path('correo_de_contacto',correo,name="Correo_de_Contacto"),
 ]
