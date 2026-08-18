89a7df60b8750be2c5653005c87dc0c27df14398
John Mario <johnygat63@gmail.com>
2026-08-18 11:38:43 -0500

    Responsive login arreglado

--- colmena/colmena/settings.py
+++ colmena/colmena/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dbmicolmena',
         'USER' : 'root',
-        'PASSWORD' : 'admin',
+        'PASSWORD' : '123456',
         'HOST' : 'localhost',
         'PORT' : '3306'
     }
